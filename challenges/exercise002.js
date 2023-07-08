0fa9714edb6d00486e7ff7c6f6fac5c0675308dd
TimalkaK <timalkamk@gmail.com>
2023-07-08 16:26:06 +0100

    feat:Completed exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,55 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
 	// Your code here!
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
 	// Your code here!
+	if (person.city === 'Manchester') {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
 	// Your code here!
+	let busNumber = people / 40;
+	if (people > 40 && people % 40 === 0) {
+		return busNumber;
+	} else if (people > 40 && people % 40 !== 0) {
+		//console.log(Math.floor(busNumber + 1));
+		return Math.floor(busNumber + 1);
+	} else {
+		return 1;
+	}
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
 	// Your code here!
+	let count = 0;
+	arr.forEach(animal => {
+		if (animal === 'sheep') {
+			count++;
+		}
+	})
+	return count;
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
 	// Your code here!
+	let pCode = person.address.postCode;
+	if (pCode.charAt(0) === 'M' && person.address.city === 'Manchester') {
+		return true;
+	} else {
+		return false;
+	}
+
+
 }
+
